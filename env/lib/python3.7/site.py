6ec703f02481d65dc7114899e8ea21b11fbe2fd9
Sourcery AI <>
2021-12-25 09:07:19 +0000

    'Refactored by Sourcery'

--- env/lib/python3.7/site.py
+++ env/lib/python3.7/site.py
@@ -115,7 +115,7 @@ def removeduppaths():
         # if they only differ in case); turn relative paths into absolute
         # paths.
         dir, dircase = makepath(dir)
-        if not dircase in known_paths:
+        if dircase not in known_paths:
             L.append(dir)
             known_paths.add(dircase)
     sys.path[:] = L
@@ -171,7 +171,7 @@ def addpackage(sitedir, name, known_paths):
                 continue
             line = line.rstrip()
             dir, dircase = makepath(sitedir, line)
-            if not dircase in known_paths and os.path.exists(dir):
+            if dircase not in known_paths and os.path.exists(dir):
                 sys.path.append(dir)
                 known_paths.add(dircase)
     finally:
@@ -190,7 +190,7 @@ def addsitedir(sitedir, known_paths=None):
     else:
         reset = 0
     sitedir, sitedircase = makepath(sitedir)
-    if not sitedircase in known_paths:
+    if sitedircase not in known_paths:
         sys.path.append(sitedir)  # Add path component
     try:
         names = os.listdir(sitedir)
@@ -258,14 +258,10 @@ def addsitepackages(known_paths, sys_prefix=sys.prefix, exec_prefix=sys.exec_pre
                 sitedirs.append(os.path.join(prefix, "lib", "dist-python"))
             else:
                 sitedirs = [prefix, os.path.join(prefix, "lib", "site-packages")]
-            if sys.platform == "darwin":
-                # for framework builds *only* we add the standard Apple
-                # locations. Currently only per-user, but /Library and
-                # /Network/Library could be added too
-                if "Python.framework" in prefix:
-                    home = os.environ.get("HOME")
-                    if home:
-                        sitedirs.append(os.path.join(home, "Library", "Python", sys.version[:3], "site-packages"))
+            if sys.platform == "darwin" and "Python.framework" in prefix:
+                home = os.environ.get("HOME")
+                if home:
+                    sitedirs.append(os.path.join(home, "Library", "Python", sys.version[:3], "site-packages"))
             for sitedir in sitedirs:
                 if os.path.isdir(sitedir):
                     addsitedir(sitedir, known_paths)
@@ -285,14 +281,18 @@ def check_enableusersite():
     if hasattr(sys, "flags") and getattr(sys.flags, "no_user_site", False):
         return False
 
-    if hasattr(os, "getuid") and hasattr(os, "geteuid"):
-        # check process uid == effective uid
-        if os.geteuid() != os.getuid():
-            return None
-    if hasattr(os, "getgid") and hasattr(os, "getegid"):
-        # check process gid == effective gid
-        if os.getegid() != os.getgid():
-            return None
+    if (
+        hasattr(os, "getuid")
+        and hasattr(os, "geteuid")
+        and os.geteuid() != os.getuid()
+    ):
+        return None
+    if (
+        hasattr(os, "getgid")
+        and hasattr(os, "getegid")
+        and os.getegid() != os.getgid()
+    ):
+        return None
 
     return True
 
@@ -413,9 +413,8 @@ class _Printer(object):
             for filename in self.__files:
                 filename = os.path.join(dir, filename)
                 try:
-                    fp = open(filename, "r")
-                    data = fp.read()
-                    fp.close()
+                    with open(filename, "r") as fp:
+                        data = fp.read()
                     break
                 except IOError:
                     pass
@@ -520,17 +519,6 @@ def setencoding():
     default is 'ascii', but if you're willing to experiment, you can
     change this."""
     encoding = "ascii"  # Default value set by _PyUnicode_Init()
-    if 0:
-        # Enable to support locale aware default string encodings.
-        import locale
-
-        loc = locale.getdefaultlocale()
-        if loc[1]:
-            encoding = loc[1]
-    if 0:
-        # Enable to switch off string to Unicode coercion and implicit
-        # Unicode to string conversion.
-        encoding = "undefined"
     if encoding != "ascii":
         # On Non-Unicode builds this will raise an AttributeError...
         sys.setdefaultencoding(encoding)  # Needs Python Unicode build !
@@ -545,13 +533,11 @@ def execsitecustomize():
 
 
 def virtual_install_main_packages():
-    f = open(os.path.join(os.path.dirname(__file__), "orig-prefix.txt"))
-    sys.real_prefix = f.read().strip()
-    f.close()
-    pos = 2
+    with open(os.path.join(os.path.dirname(__file__), "orig-prefix.txt")) as f:
+        sys.real_prefix = f.read().strip()
     hardcoded_relative_dirs = []
     if sys.path[0] == "":
-        pos += 1
+        pos = 2 + 1
     if _is_pypy:
         if sys.version_info > (3, 2):
             cpyver = "%d" % sys.version_info[0]
@@ -575,11 +561,11 @@ def virtual_install_main_packages():
         paths = [os.path.join(sys.real_prefix, "lib", "python" + sys.version[:3])]
         hardcoded_relative_dirs = paths[:]  # for the special 'darwin' case below
         lib64_path = os.path.join(sys.real_prefix, "lib64", "python" + sys.version[:3])
-        if os.path.exists(lib64_path):
-            if _is_64bit:
+        if _is_64bit:
+            if os.path.exists(lib64_path):
                 paths.insert(0, lib64_path)
-            else:
-                paths.append(lib64_path)
+        elif os.path.exists(lib64_path):
+            paths.append(lib64_path)
         # This is hardcoded in the Python executable, but relative to
         # sys.prefix.  Debian change: we need to add the multiarch triplet
         # here, which is where the real stuff lives.  As per PEP 421, in
